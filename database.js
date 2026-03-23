39b4dd1788b7fc3ce876c8e2bea6396bf519cf0f
Bianca <bibisfiha77@gmail.com>
2026-03-23 19:30:09 -0300

    feat: adiciona busca por ID (GET) e cadastro de incidentes (POST)

---
+++ database.js
@@ -0,0 +1,78 @@
+const sqlite3 = require('sqlite3')
+const { open } = require('sqlite')
+
+const criarBanco = async () => {
+
+    const db = await open({
+        filename: './database.db',
+        driver: sqlite3.Database
+    })
+
+    await db.exec(`
+    CREATE TABLE IF NOT EXISTS incidentes(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        tipo_problema TEXT,
+        localizacao TEXT,
+        descricao TEXT,
+        prioridade TEXT,
+        nome_solicitante TEXT,
+        data_registro TEXT,
+        hora_registro TEXT,
+        status_resolucao TEXT DEFAULT 'Pendente'
+    )
+    `);
+    console.log("Banco de dados configurado: A tabela de registros urbanos está pronta!");
+
+    //INSERT - C do CRUD - CREATE
+
+    const checagem = await db.get(`SELECT COUNT (*) AS total FROM incidentes`)
+
+    if (checagem.total === 0) {
+        await db.exec(`
+        INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante,
+        data_registro, hora_registro) VALUES
+        ('Iluminação', 'Rua das Flores, 123, Bairro das Margaridas', 'Poste queimado há dias', 'Média', 'Ana Clara', '16/03/2026', '10:30'),
+        ('Falta de energia', 'Hospital JP2', 'Local na escuridão', 'Alta', 'Antônio Perna Quebrada', '16/03/2026', '22:15'),
+        ('Vazamento de água', 'Rua das Camélias, 52', 'Vazamento de água constante próximo ao bueiro', 'Alta', 'Júlia Martins', '16/03/2026', '10:00'),
+        ('Pavimentação', 'Avenida C, Bairro D', 'Calçada em mau estado', 'Alta', 'Maria Oliveira', '14/03/2026', '14:30'),
+        ('Falta de água', 'Rua T, 146, Jardim Imbarie', 'Moradores sem água', 'Alta', 'Dona Fofoca', '16/03/2026', '10:00')
+    `);
+    } else {
+        console.log(`Banco pronto com ${checagem.total} de incidentes`);
+    };
+
+    //SELECT - R do CRUD - READ
+    const todosOsIncidentes = await db.all(`SELECT * FROM incidentes`);
+    console.table(todosOsIncidentes);
+
+    const chamadosAna = await db.all(`SELECT * FROM incidentes WHERE nome_solicitante = 'Ana Clara'`);
+    console.table(chamadosAna);
+
+    //UPDATE - U do CRUDE
+    await db.run(`
+        UPDATE incidentes 
+        SET status_resolucao = "Em Análise"
+        WHERE data_registro = "16/03/2026"
+        `)
+    console.log("Todas as reclamações do dia 16/03/2026 tiveram uma atualização");
+
+    await db.run(`
+        UPDATE incidentes
+        SET status_resolucao = "Resolvido"
+        WHERE tipo_problema = "Falta de energia"
+        `)
+    console.log("Problema do hospital resolvido");
+
+    //DELETE - D do CRUD
+    await db.run(`DELETE FROM incidentes WHERE id = 2`)
+    console.log("Registro do ID 2 removido");
+
+    //console.log("Relatório Atualizado(FINAL)")
+    const resultadoFinal = await db.all(`SELECT * FROM incidentes`);
+    console.table(resultadoFinal);
+
+    return db;
+
+};
+
+module.exports = { criarBanco }
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const { criarBanco } = require('./database')
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`
+        <body>
+        
+        <h1> ZelaCidade </h1>
+        <h2> Gestão de Problemas Urbanos </h2>
+        <p> Endpoint que leva aos incidentes cadastrados: /incidentes </p>
+
+        </body>
+        `);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.get('/incidentes', async (req, res) => {
+    const db = await criarBanco()
+
+    const listaIncidentes = await db.all(`SELECT * FROM incidentes`)
+
+    res.json(listaIncidentes)
+});
+
+//rota específica
+app.get('/incidentes/:id', async (req, res) => {
+    const { id } = req.params
+
+    const db = await criarBanco()
+
+    const incidenteEspecifico = await db.all(`SELECT * FROM incidentes WHERE id = ?`, [id])
+
+    res.json(incidenteEspecifico)
+});
+
+app.post('/incidentes', async (req, res) => {
+    const { tipo_problema, localizacao, descricao, prioridade, nome_solicitante,
+        data_registro, hora_registro } = req.body
+    
+    const db = await criarBanco()
+
+    await db.run(`INSERT INTO incidentes(tipo_problema, localizacao, descricao, prioridade, nome_solicitante,
+        data_registro, hora_registro) VALUES (?, ?, ?, ?, ?, ?, ?)`, [tipo_problema, localizacao, descricao, prioridade, nome_solicitante,
+        data_registro, hora_registro])
+
+    res.send(`Incidente novo resgistrado: ${tipo_problema} resgistrado na data ${data_registro} por ${nome_solicitante}`)
+});
